5ba26cfe5851bc90441f0d9f2eeeb6d2745f5d03
alihaghjou <alihaghjou30@gmail.com>
2023-07-21 14:10:06 +0330

    background color and positions added

--- src/App.tsx
+++ src/App.tsx
@@ -29,27 +29,30 @@ function App() {
     });
   };
   return (
-    <div>
+    <main className="flex flex-col justify-center items-center gap-8 py-4">
       <h1>Choose the right color:</h1>
-      <div className="w-10 h-10" style={{ backgroundColor: `#${bgColor}` }}>
-        {bgColor}
-      </div>
-      <FormColor
-        handleSubmit={handleSubmit}
-        select={select}
-        setSelect={setSelect}
-        options={options}
-      />
-      {result.isSubmited && (
-        <div>
-          {result.isTrue ? (
-            <div>You gussed right</div>
-          ) : (
-            <div>Wrong, Correcr answer is {bgColor}</div>
-          )}
-        </div>
-      )}
-    </div>
+      <section>
+        <div
+          className="w-64 h-64 mb-6 rounded"
+          style={{ backgroundColor: `#${bgColor}` }}
+        ></div>
+        <FormColor
+          handleSubmit={handleSubmit}
+          select={select}
+          setSelect={setSelect}
+          options={options}
+        />
+        {result.isSubmited && (
+          <div className="text-center py-4">
+            {result.isTrue ? (
+              <div>You gussed right</div>
+            ) : (
+              <div>Wrong, Correcr answer is <b className="underline">{bgColor}</b></div>
+            )}
+          </div>
+        )}
+      </section>
+    </main>
   );
 }
 
--- src/FormColor.tsx
+++ src/FormColor.tsx
@@ -20,23 +20,32 @@ type typeProps = {
 
 function FormColor({ handleSubmit, options, select, setSelect }: typeProps) {
   return (
-    <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col">
-      {options.map((option) => (
-        <div key={option.option}>
-          <input
-            type="radio"
-            name="color"
-            id="1"
-            value={option.option}
-            onChange={(e) =>
-              setSelect({ selected: e.target.value, isTrue: option.isTrue })
-            }
-            checked={select.selected === option.option}
-          />
-          <label htmlFor="">#{option.option}</label>
-        </div>
-      ))}
-      <button type="submit">Submit</button>
+    <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-6">
+      <article className="grid grid-cols-2 gap-4">
+        {options.map((option) => (
+          <div key={option.option} className="flex items-center justify-center">
+            <input
+            
+              type="radio"
+              name="color"
+              id="1"
+              value={option.option}
+              onChange={(e) =>
+                setSelect({ selected: e.target.value, isTrue: option.isTrue })
+              }
+              checked={select.selected === option.option}
+            />
+            <label htmlFor="">#{option.option}</label>
+          </div>
+        ))}
+      </article>
+
+      <button
+        type="submit"
+        className="ring-1 ring-cyan-900 rounded py-4 hover:bg-cyan-900 hover:text-cyan-100 text-cyan-900"
+      >
+        Submit
+      </button>
     </form>
   );
 }
